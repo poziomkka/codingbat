285e9a9e3c9b29e0398409e988e278c486263a35
m.kowalska <magdaalenaa@gmail.com>
2019-07-22 18:09:01 +0200

    added methods in String3 class: mirrorEnds(), sameEnds(), sumDigits(), countTriple(), gHappy(), equalIsNot(), withoutString(); refactor in WarmUp1 class: or35(), front22() startHi()

--- src/main/java/String3.java
+++ src/main/java/String3.java
@@ -1,5 +1,93 @@
 public class String3 {
 
+    // Given two strings, "base" and "remove", return a version of the base string where all instances of the remove
+    // string have been removed (not case sensitive). You may assume that the remove string is length 1 or more.
+    // Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+    public String withoutString(String str, String remove) {
+        StringBuilder sb = new StringBuilder();
+        int help = 0;
+        if (str.length() < remove.length()) return str;
+        for (int i = 0; i < str.length() - remove.length()+1; i++) {
+            if (!str.substring(i, i + remove.length()).equalsIgnoreCase(remove)) sb.append(str.charAt(i));
+            else i+= remove.length() - 1;
+            help = str.length() - remove.length() - i;
+        }
+        return sb.append(str.substring(str.length() - remove.length() + 1 - help, str.length())).toString();
+    }
+
+    // Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number
+    // of appearances of "not" anywhere in the string (case sensitive).
+    public boolean equalIsNot(String str) {
+        int is = 0;
+        int not = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("not")) not++;
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 2).equals("is")) is++;
+        }
+        return is==not;
+    }
+
+    // We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
+    // Return true if all the g's in the given string are happy.
+    public boolean gHappy(String str) {
+        if (str.length() == 0) return true;
+        if (str.length() == 1) return false;
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.substring(i, i + 1).equals("g") &&
+                    (str.charAt(i - 1) != 'g' && str.charAt(i + 1) != 'g')) return false;
+        }
+        return !((str.charAt(0) == 'g' && str.charAt(1) != 'g') || (str.charAt(str.length() - 1) == 'g' &&
+                str.charAt(str.length() - 2) != 'g'));
+    }
+
+    // We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples in
+    // the given string. The triples may overlap.
+    public int countTriple(String str) {
+        if (str.length() < 3) return 0;
+        int n = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)) n++;
+        }
+        return n;
+    }
+
+    // Given a string, return the sum of the digits 0-9 that appear in the string, ignoring all other characters.
+    // Return 0 if there are no digits in the string. (Note: Character.isDigit(char) tests if a char is one of the
+    // chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+    public int sumDigits(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) sum += Integer.parseInt(str.substring(i, i + 1));
+        }
+        return sum;
+    }
+
+    // Given a string, return the longest substring that appears at both the beginning and end of the string without
+    // overlapping. For example, sameEnds("abXab") is "ab".
+    public String sameEnds(String str) {
+        String p = "";
+        int a = str.length();
+        for (int i = a / 2; i > 0; i--) {
+            if (str.substring(0, i).equals(str.substring(str.length() - i, a))) return str.substring(0, i);
+        }
+        return p;
+    }
+
+    // Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+    // In other words, zero or more characters at the very begining of the given string, and at the very end
+    // of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+    public String mirrorEnds(String str) {
+        StringBuilder sb = new StringBuilder();
+        if (str.length() < 2) return str;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(str.length() - i - 1)) sb.append(str.charAt(i));
+            else break;
+        }
+        return sb.toString();
+    }
+
     // Given a string, return the length of the largest "block" in the string.
 // A block is a run of adjacent chars that are the same.
     public int maxBlock(String str) {
@@ -31,35 +119,26 @@ public class String3 {
 // followed by a letter -- so for example the "is" in "this" does not count.
 // (Note: Character.isLetter(char) tests if a char is a letter.)
     public String notReplace(String str) {
-
-        String strN = "";
+        StringBuilder sb = new StringBuilder();
         boolean front;
         boolean back;
-        int lastOne = 0;
-
         if (str.length() < 2) return str;
         for (int i = 0; i < str.length() - 1; i++) {
-            lastOne = i;
-
             if (str.substring(i, i + 2).equals("is")) {
-                lastOne = 0;
                 if (i == 0) front = true;
-                else if (!Character.isLetter(str.charAt(i - 1))) front = true;
-                else front = false;
+                else front = !Character.isLetter(str.charAt(i - 1));
 
                 if (i == str.length() - 2) back = true;
-                else if (!Character.isLetter(str.charAt(i + 2))) back = true;
-                else back = false;
+                else back = !Character.isLetter(str.charAt(i + 2));
 
-                if (front && back) strN += "is not";
-                else strN += "is";
+                if (front && back) sb.append("is not");
+                else sb.append("is");
+
                 i++;
-            } else strN += str.charAt(i);
-
-            if (i == str.length() - 2) strN += str.charAt(i + 1);
+            } else sb.append(str.charAt(i));
+            if (i == str.length() - 2) sb.append(str.charAt(i + 1));
         }
-        return strN;
+        return sb.toString();
     }
-
 
 }
--- src/main/java/Warmup1.java
+++ src/main/java/Warmup1.java
@@ -2,15 +2,14 @@ public class Warmup1 {
     // Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
 // Use the % "mod" operator
     public boolean or35(int n) {
-        if (n % 5 == 0 || n % 3 == 0) return true;
-        return false;
+        return (n % 5 == 0 || n % 3 == 0);
     }
 
     // Given a string, take the first 2 chars and return the string with the 2 chars added
 // at both the front and back, so "kitten" yields"kikittenki".
 // If the string length is less than 2, use whatever chars are there.
     public String front22(String str) {
-        if (str == "") return "";
+        if (str.equals("")) return "";
         if (str.length() == 1) return str + str + str;
         return str.substring(0, 2) + str + str.substring(0, 2);
     }
@@ -18,8 +17,7 @@ public class Warmup1 {
     // Given a string, return true if the string starts with "hi" and false otherwise.
     public boolean startHi(String str) {
         if (str.length() < 2) return false;
-        if (str.substring(0, 2).equals("hi")) return true;
-        return false;
+        return (str.substring(0, 2).equals("hi"));
     }
 }
 
